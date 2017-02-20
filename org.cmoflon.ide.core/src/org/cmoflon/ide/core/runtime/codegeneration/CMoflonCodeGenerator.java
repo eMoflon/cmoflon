215685736177a883fa1cb6806807b8b2cbe30848
David Giessing <david.giessing93@gmail.com>
2017-02-20 17:55:06 +0100

    Typo

--- org.cmoflon.ide.core/src/org/cmoflon/ide/core/runtime/codegeneration/CMoflonCodeGenerator.java
+++ org.cmoflon.ide.core/src/org/cmoflon/ide/core/runtime/codegeneration/CMoflonCodeGenerator.java
@@ -139,9 +139,9 @@ public class CMoflonCodeGenerator
    												+"\t\t}"+nl()
    												+"\t\tprintf(\"[topologycontrol]: DEGREE: %d\\n\",degree);"+nl()
    												+"\t\tunsigned long start=RTIMER_NOW();"+nl()
-   												+"\t\tprintf(\"[topologycontrol]: STATUS: Run\n\");"+nl();
+   												+"\t\tprintf(\"[topologycontrol]: STATUS: Run\\n\");"+nl();
    
-   private static String EVAL_STATEMENTS_END = "printf(\"[topologycontrol]: TIME: %lu\\n\",RTIMER_NOW()-start);"+nl();
+   private static String EVAL_STATEMENTS_END = "\t\tprintf(\"[topologycontrol]: TIME: %lu\\n\",RTIMER_NOW()-start);"+nl();
 
    /**
     * Constants mapping
