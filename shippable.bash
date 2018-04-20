04989f4e08936e0c39827ea76b9a7c3d8c39de8c
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-04-20 10:30:36 +0200

    Initial commit of CI setup (scripts/pom.xml's/...)

---
+++ shippable.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script contains the CI build workflow for this repository
+# Author: Roland Kluge
+# Date: 2018-04-20
+#
+
+# Change into Tycho root parent directory
+cd ./src/
+
+echo "Run Tycho (clean compile)"
+mvn -q clean compile integration-test || exit -1
+
+echo "Publish JUnit test results"
+find . -path "*/target/*/TEST*.xml" -exec cp {} /root/src/github.com/eMoflon/emoflon-core/shippable/testresults/ \;
